8520f1e747dcf53923845f0049079160c28aef87
Raina Freeman <90727927+raf1843@users.noreply.github.com>
2022-10-02 09:00:51 -0400

    Hopefully uploading properly this time
    Added some formatting and output

--- project1_executables/apm.sh
+++ project1_executables/apm.sh
@@ -6,11 +6,19 @@
 
 # Spawn processes
 spawn(){
-  #need "ifstat -d |"
+  # hardcoded gateway address
+  gway='192.168.182.2'
+  ifstat -d 1
+  # start processes and csv files
   for((i=1; $i <= 6; i++))
   do
-    echo $i
+    psname="APM${i}"
+    chmod 755 $psname
+    ./$psname $gway &
+    echo Seconds,%CPU,%Memory > "${psname}_metrics.csv"
   done 
+  # also start csv file for system
+  echo Seconds,RX Data Rate,TX Data Rate,Disk Writes,Available Disk Capacity > system_metrics.csv
 }
 
 # Collect process metrics
@@ -20,22 +28,32 @@ collect_ps(){
 
 # Collect system metrics
 collect_sys(){
-	net_rrate=$(ifstat ens33 | sed -n '4p' | xargs | cut -f 7 -d ' ')
-	net_trate=$(ifstat ens33 | sed -n '4p' | xargs | cut -f 9 -d ' ')
-	
-	hd_write=$(iostat sda | sed -n '7p' | xargs | cut -f 4 -d ' ')
-
-	hd_util=$(df -hm / | sed -n '2p' | xargs | cut -f 4 -d ' ')
+  net_rrate=$(ifstat -a ens33 2>/dev/null | sed -n '4p' | xargs | cut -f 7 -d ' ')
+  net_trate=$(ifstat -a ens33 2>/dev/null | sed -n '4p' | xargs | cut -f 9 -d ' ')
+  hd_write=$(iostat sda | sed -n '7p' | xargs | cut -f 4 -d ' ')
+  hd_util=$(df -hm / | sed -n '2p' | xargs | cut -f 4 -d ' ')
+  echo "$SECONDS,$net_rrate,$net_trate,$hd_write,$hd_util" >> system_metrics.csv
 }
 
 # Clean up
 cleanup(){
-  echo
+  pkill ifstat
+  for((i=1; $i <= 6; i++))
+  do
+    psname="APM${i}"
+    pkill $psname  # would like to figure out how to make this silent
+  done
+  echo "Check output in <ps_name>_metrics.csv and system_metrics.csv files"
 }
 trap cleanup EXIT
 
 # main
 
 spawn
-collect_ps
-collect_sys
+while true
+do
+  sleep 5
+  collect_ps
+  collect_sys
+done
+
